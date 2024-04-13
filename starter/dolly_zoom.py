3905856bf10a0bb5e6bcb0c7c4699fcd2149af75
akasparm <akabuoy1999@gmail.com>
2024-04-12 23:40:36 -0400

    Dolly zoom effect

--- starter/dolly_zoom.py
+++ starter/dolly_zoom.py
@@ -1,6 +1,6 @@
 """
 Usage:
-    python -m starter.dolly_zoom --num_frames 10
+    python -m starter.dolly_zoom
 """
 
 import argparse
@@ -18,9 +18,8 @@ from starter.utils import get_device, get_mesh_renderer
 def dolly_zoom(
     image_size=256,
     num_frames=10,
-    duration=3,
+    duration=1,
     device=None,
-    output_file="output/dolly.gif",
 ):
     if device is None:
         device = get_device()
@@ -33,9 +32,10 @@ def dolly_zoom(
     fovs = torch.linspace(5, 120, num_frames)
 
     renders = []
+
     for fov in tqdm(fovs):
-        distance = 3  # TODO: change this.
-        T = [[0, 0, 3]]  # TODO: Change this.
+        distance = 5/(2*np.tan(np.deg2rad(fov/2)))  # TODO: change this.
+        T = [[0, 0, distance]]  # TODO: Change this.
         cameras = pytorch3d.renderer.FoVPerspectiveCameras(fov=fov, T=T, device=device)
         rend = renderer(mesh, cameras=cameras, lights=lights)
         rend = rend[0, ..., :3].cpu().numpy()  # (N, H, W, 3)
@@ -47,19 +47,17 @@ def dolly_zoom(
         draw = ImageDraw.Draw(image)
         draw.text((20, 20), f"fov: {fovs[i]:.2f}", fill=(255, 0, 0))
         images.append(np.array(image))
-    imageio.mimsave(output_file, images, fps=(num_frames / duration))
+    imageio.mimsave('results/dolly_zoom.gif', images, duration = duration, loop=0)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_frames", type=int, default=10)
-    parser.add_argument("--duration", type=float, default=3)
-    parser.add_argument("--output_file", type=str, default="images/dolly.gif")
+    parser.add_argument("--duration", type=float, default=1)
     parser.add_argument("--image_size", type=int, default=256)
     args = parser.parse_args()
     dolly_zoom(
         image_size=args.image_size,
         num_frames=args.num_frames,
         duration=args.duration,
-        output_file=args.output_file,
     )
